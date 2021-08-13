a0bdc1d7358e86e6e6212e119d8c81d250ebf210
Adam Makarucha <adamjm@au1.ibm.com>
2021-08-13 15:02:48 +1000

    fised request post call

--- app.py
+++ app.py
@@ -130,8 +130,8 @@ def response_from_server(url, image_file, verbose=True):
     Returns:
         requests.models.Response: Response from the server.
     """
-    files = {'img': ('test.jpeg', image_file, 'image/jpeg')}
-    # files = {'file': image_file, ''}
+    #files = {'img': ('test.jpeg', image_file, 'image/jpeg')}
+    files = {'file': image_file}
     response = requests.post(url, files=files)
     status_code = response.status_code
     if verbose:
